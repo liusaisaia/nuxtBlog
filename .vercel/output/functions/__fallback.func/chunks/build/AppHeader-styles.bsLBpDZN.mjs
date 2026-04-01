9cfb9c3386d07a51abe055ba7bb8350e34b856bc
saisai.liu <liusaisai@jtexpress.com>
2026-04-01 09:35:19 +0800

    chore: 更新.gitignore文件，添加.vercel目录忽略
    清理Vercel构建输出文件，删除静态资源及无用文件

--- .vercel/output/functions/__fallback.func/chunks/build/AppHeader-styles.bsLBpDZN.mjs
+++
@@ -1,8 +0,0 @@
-const AppHeader_vue_vue_type_style_index_0_scoped_f8668f76_lang = ".theme-toggle svg[data-v-f8668f76]{color:var(--text-soft)}.theme-toggle:hover svg[data-v-f8668f76]{color:var(--text)}";
-
-const AppHeaderStyles_bsLBpDZN = [
-  AppHeader_vue_vue_type_style_index_0_scoped_f8668f76_lang
-];
-
-export { AppHeaderStyles_bsLBpDZN as default };
-//# sourceMappingURL=AppHeader-styles.bsLBpDZN.mjs.map
--- .vercel/output/functions/__fallback.func/chunks/build/composables-DdquGXk6.mjs
+++
@@ -1,8 +0,0 @@
-import { m as useState } from './server.mjs';
-
-const useColorMode = () => {
-  return useState("color-mode").value;
-};
-
-export { useColorMode as u };
-//# sourceMappingURL=composables-DdquGXk6.mjs.map
